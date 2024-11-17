655bca476e245841d93aa0a8434748f43c01d08d
MishelKV <mishelkvGH@gmail.com>
2024-11-17 19:54:21 +0300

    Modul_3

---
+++ fake_math.py
@@ -0,0 +1,13 @@
+def divide(first , second):
+    if second==0:
+        division = 'Ошибка'
+        return division
+    else:
+        division = first / second
+        return division
+
+def main():
+    print('Тест')
+
+if __name__ == '__main__':
+    main()
---
+++ true_math.py
@@ -0,0 +1,15 @@
+from math import inf
+
+def divide(first , second):
+    if second==0:
+        return inf
+    else:
+        division = first / second
+        return division
+
+def main():
+    print('Тест')
+
+if __name__ == '__main__':
+    main()
+
