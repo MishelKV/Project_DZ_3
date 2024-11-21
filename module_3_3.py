d129f68e5b1c6e48ca8cd05de273271eb7c38da8
MishelKV <mishelkvGH@gmail.com>
2024-11-21 20:33:51 +0300

    Modul_3

--- module_3_3.py
+++ module_3_3.py
@@ -1,17 +1,18 @@
 def print_params(a = 1, b = 'Строка', c = True):
-    print(a, b, c)
 
-print_params()
-print_params(2)
-print_params(2,'True')
-print_params(b = 25)
-print_params(c = [1,2,3])
+    return (a, b, c)
+
+print(print_params())
+print(print_params(2))
+print(print_params(2,'True'))
+print(print_params(b = 25))
+print(print_params(c = [1,2,3]))
 
 values_list = [1, 'Строка', True]
-values_dict = {"Mikhail": 1976, "Denis": 'Преподаватель', "Artur": True }
+values_dict = {"a": 1976, "b": 'Преподаватель', "c": True }
 
-print_params(values_list)
-print_params(values_dict)
+print(print_params(*values_list))
+print(print_params(**values_dict))
 
 values_list_2 = [54.32, 'Строка']
-print_params(*values_list_2,42)
+print(print_params(*values_list_2,42))
