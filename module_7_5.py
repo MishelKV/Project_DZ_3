710f444b5012d4f75ffa97396784cae4e84e8cbd
MishelKV <mishelkvGH@gmail.com>
2025-01-03 19:53:30 +0300

    Modul_3

---
+++ module_7_5.py
@@ -0,0 +1,27 @@
+import os
+import time
+
+if os.path.exists('foldermodule7'):
+    os.chdir('foldermodule7')
+else:
+    os.mkdir('foldermodule7')
+    os.chdir('foldermodule7')
+
+directory = '.'
+
+for root, dirs, files in os.walk(directory):
+    for file in files:
+        filepath = os.path.join(file)
+        filetime = os.path.getmtime(file)
+        formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
+        filesize = os.path.getsize(file)
+        parent_dir = os.path.dirname(file)
+        current_dir = os.getcwd()
+
+        print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time},'
+              f' Родительская директория: {parent_dir}')
+
+        print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time},'
+              f' Родительская директория: {current_dir}')
+
+
